72e3004dcb7d597a9b44c07ebe575d8a121108c6
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2023-02-03 13:06:46 +0100

    v2.1 <- Stage (#79)
    * fix: update dataprovider data
    
    * fix: getExpectedOutput now take collateral used to repay debt into account
    
    * fix: remove skeleton infinite loading on entering and exiting
    
    * feat: Flatten repo, use modern build tooling (#56)
    
    * feat: wallet connect upgrade
    
    * fix: enable loose
    
    * clean
    
    * mv: bring folder back
    
    * remove .js postfix on css or scss file imports
    
    * temp: vite
    
    * feat: migrate to vite
    
    * docker build
    
    * update terraform and bootstrap
    
    * fix: merge cleanup
    
    * simlink: barretenberg
    
    * fix: config issue
    
    * chore: bump sdk
    
    * lint
    
    * clean: remove sims link with vite
    
    * lint
    
    * chore: enable pnp again
    
    * clean: bootstrap changes
    
    * fix: move tf folder back into zk-money (#68)
    
    * fix: remove dangling files (#69)
    
    * Add LUSD yearn vault (#67)
    
    * fix: add LUSD yearn vault
    
    * fix: LUSD icons
    
    * fix: set `openHandleAssetBinding = 'yvLUSD'`
    
    * fix: update raw data (#70)
    
    * fix: update raw data
    
    * fix: add yvLUSD to test and stage.
    
    * Hide redundant defi recipient (#65)
    
    * fix: set `showUnderlying = false`.
    
    * fix: Hide recipient from form on defi deposits
    
    * fix: undo unrelated commit
    
    * fix: set `showUnderlying = false`. (#66)
    
    * Remove flag for liquity and compound + fix binding (#71)
    
    * fix: update gas usage liquity bridge
    
    * fix: remove flag from compound and only show 1 liquity bridge
    
    * fix: wallet connect regression due to node polyfills (#72)
    
    * Approve tx instead of permit for dai (#73)
    
    * fix: Approve tx instead of permit for dai
    
    * fix: Remove KNOWN_MAINNET_ASSET_ADDRESS from shield_composer
    
    * fix: Update sorting + remove set from recipes list
    
    * fix: Update compound APR computation. (#77)
    
    undefined
    
    ---------
    
    Co-authored-by: Maddiaa <47148561+cheethas@users.noreply.github.com>

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:18-alpine
+RUN apk update && apk add --no-cache git curl bash
+
+COPY zk-money zk-money
+COPY lambda lambda
+
+WORKDIR zk-money
+RUN yarn && yarn build && yarn formatting
+
+WORKDIR ../lambda
+RUN apk add zip
+RUN yarn && yarn build && yarn && yarn build:zip
+
+FROM node:18-alpine
+COPY --from=0 zk-money /usr/src/zk-money
+COPY --from=0 lambda /usr/src/lambda
+
+WORKDIR usr/src/zk-money
+CMD ["yarn", "start"]
+EXPOSE 8080
---
+++ zk-money/src/views/account/dashboard/register_account_form.tsx
@@ -0,0 +1,173 @@
+import { useContext, useEffect } from 'react';
+import { AmountSelection, WalletInteractionStep } from '../../../components/index.js';
+import { Field, FieldStatus, TxProgressStep, FormWarning } from '../../../ui-components/index.js';
+import { RegisterForm } from '../../../alt-model/forms/register/register_form_hooks.js';
+import { RegisterFormFlowRunnerState } from '../../../alt-model/forms/register/register_form_flow_runner_hooks.js';
+import { TopLevelContext } from '../../../alt-model/top_level_context/top_level_context.js';
+import { TxGasSection } from './modals/sections/gas_section/index.js';
+import { getRegisterToast } from '../../toasts/toast_configurations.js';
+import { useWalletInteractionIsOngoing } from '../../../alt-model/wallet_interaction_hooks.js';
+import { Amount } from '../../../alt-model/assets/amount.js';
+import style from './register_account_form.module.scss';
+
+export type KeyType = 'account' | 'spending';
+export type PhaseType = 'idle' | 'signer-select' | 'awaiting-signature';
+
+interface RegisterAccountFormProps {
+  registerForm: RegisterForm;
+  locked: boolean;
+  runnerState: RegisterFormFlowRunnerState;
+  onResetRunner: () => void;
+  onRetry: () => Promise<void>;
+  onCancel: () => void;
+}
+
+function getAliasFieldStatus(alias: string, aliasFeedback?: string, checkingAlias?: boolean) {
+  const aliasHasFeedback = aliasFeedback && aliasFeedback.length > 0;
+
+  if (checkingAlias) {
+    return FieldStatus.Loading;
+  }
+
+  if (aliasHasFeedback) {
+    return FieldStatus.Error;
+  }
+
+  if (alias.length > 0) {
+    return FieldStatus.Success;
+  }
+}
+
+function getInteractionItem(runnerState: RegisterFormFlowRunnerState) {
+  const { flowState } = runnerState;
+  if (runnerState.error) {
+    return {
+      interactionStep: WalletInteractionStep.Error,
+    };
+  }
+
+  switch (flowState?.phase) {
+    case 'creating-proof':
+      return {
+        step: TxProgressStep.PROVING,
+        interactionStep: WalletInteractionStep.CreatingProof,
+      };
+    case 'deposit-and-sign':
+      return {
+        step: TxProgressStep.SIGNING_L1_DEPOSIT,
+        interactionStep: WalletInteractionStep.L1DepositAndSignInteractions,
+      };
+    case 'sending-proof':
+      return {
+        step: TxProgressStep.SIGNING_L1_DEPOSIT,
+        interactionStep: WalletInteractionStep.SendingProof,
+      };
+    case 'done':
+      return {
+        step: TxProgressStep.DONE,
+        interactionStep: WalletInteractionStep.Done,
+      };
+  }
+}
+
+export function RegisterAccountForm(props: RegisterAccountFormProps) {
+  const { fields, setters, feedback, assessment, resources, locked } = props.registerForm;
+  const { walletInteractionToastsObs } = useContext(TopLevelContext);
+  const walletInteractionIsOngoing = useWalletInteractionIsOngoing();
+
+  const feeAmount = fields.speed !== null ? resources.feeAmounts?.[fields.speed] : undefined;
+  const pendingAmount = resources.l1PendingBalance
+    ? new Amount(resources.l1PendingBalance, resources.depositAsset)
+    : undefined;
+
+  useEffect(() => {
+    const interactionItem = getInteractionItem(props.runnerState);
+    if (interactionItem) {
+      const toastItem = getRegisterToast(
+        props.onRetry,
+        props.onCancel,
+        props.onResetRunner,
+        walletInteractionToastsObs,
+        props.runnerState,
+        interactionItem,
+      );
+
+      walletInteractionToastsObs.addOrReplaceToast(toastItem);
+      return () => walletInteractionToastsObs.removeToastByKey(toastItem.key);
+    }
+  }, [props.onRetry, props.onResetRunner, props.onCancel, walletInteractionToastsObs, props.runnerState]);
+
+  const handleUsePendingFunds = () => {
+    if (!pendingAmount || fields.speed === null || !feeAmount) return;
+    const pendingAmountMinusFee = pendingAmount.subtract(feeAmount?.baseUnits);
+    setters.depositValueStrOrMax(
+      pendingAmountMinusFee.format({
+        layer: 'L1',
+        uniform: true,
+        hideSymbol: true,
+        hideComma: true,
+      }),
+    );
+  };
+
+  return (
+    <div className={style.registerAccountForm}>
+      <Field
+        label="Pick an alias"
+        sublabel={
+          <>
+            Choose an alias in place of your account key so other users can find you more easily.{' '}
+            <span style={{ fontWeight: 450 }}>Warning: Aliases are case sensitive!</span>
+          </>
+        }
+        value={fields.alias}
+        onChangeValue={(value: string) => setters.alias(value)}
+        disabled={locked || walletInteractionIsOngoing}
+        placeholder="@username"
+        prefix="@"
+        message={feedback.alias}
+        status={getAliasFieldStatus(fields.alias, feedback.alias, resources.checkingAlias)}
+      />
+      <TxGasSection
+        speed={fields.speed}
+        onChangeSpeed={setters.speed}
+        feeAmounts={resources.feeAmounts}
+        disabled={locked}
+        label={'Select a speed for your registration'}
+        balanceType="L1"
+        asset={resources.depositAsset}
+        targetAssetIsErc20={fields.depositAssetId !== 0}
+      />
+      <AmountSelection
+        label={'Make your first deposit (Optional)'}
+        sublabel={'Take advantage of your registration transaction fee and make a feeless deposit'}
+        maxAmount={assessment.balances?.info.maxL2Output ?? 0n}
+        asset={resources.depositAsset}
+        feeAmount={feeAmount}
+        allowWalletSelection={true}
+        amountStringOrMax={fields.depositValueStrOrMax}
+        disabled={locked}
+        allowAssetSelection={false}
+        onChangeAsset={setters.depositAssetId}
+        message={feedback.deposit}
+        onChangeAmountStringOrMax={setters.depositValueStrOrMax}
+        balanceType={'L1'}
+      />
+      {feedback.amount && <FormWarning text={feedback.amount} />}
+      {feedback.walletAccount && <FormWarning text={feedback.walletAccount} />}
+      {feedback.footer && <FormWarning text={feedback.footer} />}
+      {feedback.pendingFunds && (
+        <FormWarning
+          text={
+            <span className={style.pendingFundsFooter}>
+              {feedback.pendingFunds}{' '}
+              <span onClick={handleUsePendingFunds} className={style.usePendingFunds}>
+                Click here to use it.
+              </span>
+            </span>
+          }
+        />
+      )}
+    </div>
+  );
+}
