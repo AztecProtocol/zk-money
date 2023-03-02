c9d39ef818a54ea09b593d763b6ce2104b7fd45e
jonybur <jobur93@gmail.com>
2023-03-02 14:47:39 +0000

    Remove export

--- src/ui-components/components/index.ts
+++ src/ui-components/components/index.ts
@@ -23,7 +23,6 @@ export * from './search_input/index.js';
 export * from './navbar/index.js';
 export * from './faq_hint/index.js';
 export * from './hyperlink/index.js';
-export * from './button/index.js';
 export * from './select/index.js';
 export * from './icons/index.js';
 export * from './icon_buttons/index.js';
