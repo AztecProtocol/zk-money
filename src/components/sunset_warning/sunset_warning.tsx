7cae8659c935409888475803d4a2edec0ff5eedc
jonybur <jobur93@gmail.com>
2023-03-21 14:01:38 +0000

    Update wording

--- src/components/sunset_warning/sunset_warning.tsx
+++ src/components/sunset_warning/sunset_warning.tsx
@@ -7,13 +7,13 @@ export function SunsetWarning() {
       className={style.formWarning}
       text={
         <div className={style.text}>
-          As of March 13th, 2023, we are sunsetting zk.money.{' '}
-          <b>Deposits will remain open until 14:00 UTC on Match 21st, 2023.</b>
-          <br /> Withdrawals can be made normally until 21st March 2024. All user deposits will remain safe.{' '}
+          We are sunsetting zk.money. Deposits are now closed, and withdrawals will be enabled for one year.
+          <br />
+          You have until March 31st, 2024 to withdraw with no fees.{' '}
           <Hyperlink
             className={style.link}
             href="https://medium.com/aztec-protocol/sunsetting-aztec-connect-a786edce5cae"
-            label={'Read our announcement here.'}
+            label={'Read our full announcement here.'}
           />
         </div>
       }
